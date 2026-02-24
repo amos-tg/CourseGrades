193826a8cf6bf2c8b6237ad74aa2ee75c7930963
Amos Thacker-Gwaltney <amostg3613@gmail.com>
2026-02-24 10:31:35 -0500

    expected behavior modeling in main, fn sigs

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <filesystem>
 
-int main(void) {
-  std::cout << "Hello, Amos!" << std::endl;
+using namespace std;
+
+
+struct Student
+{
+  string name;
+  double average_score;
+  int id;
+  char grade;
+};
+
+Student* getStudentData(const filesystem::path &data_path);
+
+void calcAverages(Student* student_data);
+
+void letterGrade(Student* Student_data);
+
+void printReport(const Student* Student_data);
+
+int main(void) 
+{
+  getStudentData(
 
   return 0;
 }
